1cdacd9a5d6e7363888330695e50fbc8e4a6d8e3
mzkmnk <sma0729mex@gmail.com>
2024-08-29 22:21:49 +0900

    add:rules,ngrx config all

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,42 @@ import eslint from '@eslint/js';
 import tsEslint from 'typescript-eslint';
 import angular from 'angular-eslint';
 import gitignore from 'eslint-config-flat-gitignore'
+import ngrx from '@ngrx/eslint-plugin/v9/index.js';
 
 export default tsEslint.config(
   gitignore(),
   // eslint typescript-eslint recommended config
   {
-    files:['**/*.ts'],
+    files:['**/*.{ts,js,jsx}'],
+    languageOptions:{
+      parser:tsEslint.parser,
+      parserOptions:{
+        project:'./tsconfig.json',
+      }
+    },
     extends: [
       eslint.configs.recommended,
       ...tsEslint.configs.strict,
       ...tsEslint.configs.stylistic,
     ],
+  },
+
+  // eslint config
+  {
+    files:["**/*.ts"],
+    rules:{
+      '@typescript-eslint/explicit-function-return-type':'error',
+      "@typescript-eslint/prefer-for-of": "error",
+      "@typescript-eslint/consistent-type-definitions": [
+        "error",
+        "type"
+      ]
+    }
+  },
+
+  // typescript-eslint config
+  {
+    files:["**/*.ts"],
   },
 
   // angular ts config
@@ -38,4 +63,12 @@ export default tsEslint.config(
       '@angular-eslint/template/no-negated-async': 'off',
     },
   },
+
+  // ngrx config
+  {
+    files:["**/*.ts"],
+    extends:[
+      ...ngrx.configs.all,
+    ]
+  }
 )
