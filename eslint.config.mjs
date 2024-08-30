c95e488116dd07b49dc9022bc0efd3848814d9e1
mzkmnk <sma0729mex@gmail.com>
2024-08-30 22:51:39 +0900

    add:eslint stylistic rules and more...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,17 @@ import tsEslint from 'typescript-eslint';
 import angular from 'angular-eslint';
 import gitignore from 'eslint-config-flat-gitignore'
 import ngrx from '@ngrx/eslint-plugin/v9/index.js';
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default tsEslint.config(
   gitignore(),
   // eslint typescript-eslint recommended config
   {
-    files:['**/*.{ts,js,jsx}'],
-    languageOptions:{
-      parser:tsEslint.parser,
-      parserOptions:{
-        project:'./tsconfig.json',
+    files           : [ '**/*.ts' ],
+    languageOptions : {
+      parser        : tsEslint.parser,
+      parserOptions : {
+        project: './tsconfig.json',
       }
     },
     extends: [
@@ -22,40 +23,132 @@ export default tsEslint.config(
     ],
   },
 
+  // eslint stylistic
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/array-bracket-spacing' : [ "error", "always" ],
+      '@stylistic/js/arrow-spacing'         : 'error',
+      '@stylistic/js/block-spacing'         : 'error',
+      '@stylistic/js/array-bracket-newline' : [ "error", "consistent" ],
+      '@stylistic/js/brace-style'           : [
+        "error",
+        "1tbs",
+        {
+          "allowSingleLine": true
+        } 
+      ],
+      '@stylistic/js/comma-style'  : [ "error", "last" ],
+      '@stylistic/js/dot-location' : [ "error", "object" ],
+      "indent"                     : [ "error", 2 ],
+      "@stylistic/js/key-spacing"  : [ "error", {
+        "multiLine": {
+          "beforeColon" : false,
+          "afterColon"  : true
+
+        },
+        "align": {
+          "beforeColon" : true,
+          "afterColon"  : true,
+          "on"          : "colon"
+        }
+      } ],
+      "@stylistic/js/max-len"           : [ "error", { "code": 80 } ],
+      "@stylistic/js/multiline-ternary" : [ "error", "always" ],
+    }
+  },
+
   // eslint config
   {
-    files:["**/*.ts"],
-    rules:{
-      '@typescript-eslint/explicit-function-return-type':'error',
-      "@typescript-eslint/prefer-for-of": "error",
-      "@typescript-eslint/consistent-type-definitions": [
-        "error",
-        "type"
-      ]
+    files : [ "**/*.ts" ],
+    rules : {
+      quotes             : [ "error", "single" ],
+      'quote-props'      : [ 'error', 'consistent-as-needed', { 'keywords': true } ],
+      'object-shorthand' : [ 'error', 'always', { 'avoidQuotes': true } ],
     }
   },
 
   // typescript-eslint config
   {
-    files:["**/*.ts"],
+    files : [ "**/*.ts" ],
+    rules : {
+      '@typescript-eslint/explicit-function-return-type' : 'error',
+      "@typescript-eslint/prefer-for-of"                 : "error",
+      "@typescript-eslint/consistent-type-definitions"   : [
+        "error",
+        "type"
+      ],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector : [ "parameter" ],
+          format   : [ "camelCase" ]
+        },
+        {
+          selector : [ "interface" ],
+          format   : [ "PascalCase" ],
+          prefix   : [ "I" ],
+        }
+      ]
+    }
+  },
+
+  // api file config
+  {
+    files : [ "**/*.api.ts " ],
+    rules : {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector : [ "class" ],
+          format   : [ "PascalCase" ],
+          suffix   : [ "API" ],
+        },
+        {
+          selector : [ "classMethod" ],
+          format   : [ "camelCase" ],
+        }
+      ]
+    }
+  },
+
+  // service file config
+  {
+    files : [ "**/*.service.ts " ],
+    rules : {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector : [ "class" ],
+          format   : [ "PascalCase" ],
+          suffix   : [ "SERVICE" ],
+        },
+        {
+          selector : "classMethod",
+          format   : [ "camelCase" ],
+        }
+      ]
+    }
   },
 
   // angular ts config
   {
-    files:['**/*.ts'],
-    extends:[
+    files   : [ '**/*.ts' ],
+    extends : [
       ...angular.configs.tsRecommended,
     ],
-    processor: angular.processInlineTemplates,
-    rules: {
+    processor : angular.processInlineTemplates,
+    rules     : {
       '@angular-eslint/no-input-rename': 'off',
     },
   },
 
   // angular html config
   {
-    files: ['**/*.html'],
-    extends: [
+    files   : [ '**/*.html' ],
+    extends : [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
     ],
@@ -66,8 +159,8 @@ export default tsEslint.config(
 
   // ngrx config
   {
-    files:["**/*.ts"],
-    extends:[
+    files   : [ "**/*.ts" ],
+    extends : [
       ...ngrx.configs.all,
     ]
   }
